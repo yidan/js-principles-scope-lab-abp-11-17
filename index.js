5e8f9bafa4a417a8bbe1035a8a52f7fe53b6250d
Yidan Wang <wangyidan621@gmail.com>
2017-11-11 19:36:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+  customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "bob"
+}
+
+const leastFavoriteCustomer ="steven"
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "john"
+}
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "mamma";
+    let favoriteCustomer = "kakak";
+  
+}
