fededcaead0df734b9c1d769ac601e97242bee43
Yidan Wang <wangyidan621@gmail.com>
2017-11-11 19:38:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'bob';
 function upperCaseCustomerName(){
-  return customerName.toUpperCase();
+   customerName =  customerName.toUpperCase();
 }
 
 function setBestCustomer(){
